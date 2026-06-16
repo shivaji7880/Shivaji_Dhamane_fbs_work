27895a1d007d2aad4e9273b77ab1c740770f2c24
shivaji7880 <shivajidhamane83@gmail.com>
2026-06-17 01:10:51 +0700

    Update Q_4.py

--- core python/Assignments/Assignment_3/Q_4.py
+++ core python/Assignments/Assignment_3/Q_4.py
@@ -1,1 +1,14 @@
-print (10>3)
+# Write a program to input all sides of triangle and check wheather triangle is valid or not.
+# Taking inputs
+
+s1=int(input('Enter side 1:'))
+s2=int(input('Enter side 2:'))
+s3=int(input('Enter side 3:'))
+
+# Perform operation
+
+if (s1+s2)>s3 and (s1+s3)>s2 and (s3+s2)>s1:
+    print('Valid triangle')
+    
+else:
+    print('Invalid triangle')
