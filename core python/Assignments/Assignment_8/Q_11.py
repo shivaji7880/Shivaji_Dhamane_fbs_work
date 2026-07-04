e55ac547105f6ca573701bf9b34e851136815978
shivaji7880 <shivajidhamane83@gmail.com>
2026-07-04 13:30:58 +0700

    Update Q_11.py

--- core python/Assignments/Assignment_8/Q_11.py
+++ core python/Assignments/Assignment_8/Q_11.py
@@ -7,21 +7,21 @@ def noOfdigits(num):
         i+=1
     return i
 
-def addition(num,digits):
+def addition(num):
     sum=0
+    digits=noOfdigits(num)
     while num>0:
       d=num%10
       sum=sum+d**digits
       num//=10
-    print (sum)
+    return sum
 
-def isArmstrong(num,digits):
-    if num==addition(num,digits):
+def isArmstrong(num):
+    if num==addition(num):
         return True
     else:
         return False
       
 num=int(input('Enter a number:'))
-digits=noOfdigits(num)
-res=isArmstrong(num,digits)
+res=isArmstrong(num)
 print(res)
